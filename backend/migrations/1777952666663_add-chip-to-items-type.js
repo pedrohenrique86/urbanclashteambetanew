54acfca7a31e19febee4366c32c13f50092d900a
Pedro Henrique <prodrigues42@gmail.com>
2026-05-05 20:02:08 -0300

    fix(chat): corrige comportamento em chat

--- backend/migrations/1777952666663_add-chip-to-items-type.js
+++ backend/migrations/1777952666663_add-chip-to-items-type.js
@@ -2,6 +2,9 @@ exports.up = (pgm) => {
   pgm.sql(`
     ALTER TABLE items DROP CONSTRAINT chk_items_type;
     ALTER TABLE items ADD CONSTRAINT chk_items_type CHECK (type IN ('weapon', 'equipment', 'shield', 'consumable', 'chip'));
+
+    ALTER TABLE items DROP CONSTRAINT chk_items_rarity;
+    ALTER TABLE items ADD CONSTRAINT chk_items_rarity CHECK (rarity IN ('common', 'uncommon', 'rare', 'epic', 'legendary', 'mythic'));
   `);
 };
 
@@ -9,5 +12,8 @@ exports.down = (pgm) => {
   pgm.sql(`
     ALTER TABLE items DROP CONSTRAINT chk_items_type;
     ALTER TABLE items ADD CONSTRAINT chk_items_type CHECK (type IN ('weapon', 'equipment', 'shield', 'consumable'));
+
+    ALTER TABLE items DROP CONSTRAINT chk_items_rarity;
+    ALTER TABLE items ADD CONSTRAINT chk_items_rarity CHECK (rarity IN ('common', 'rare', 'legendary'));
   `);
 };
